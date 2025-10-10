c614ffaca5c8c07ccb82bcc8e918085db2d93657
TajanaSA <tajana.salkic@skyline.be>
2025-10-10 13:08:35 +0200

    Added ElementSelectorModel that implemets IElementSelector, loads the elements from IDms, automatically selects the first element if another element is not selected, also added IElementSelector interface

---
+++ Automation_1/ElementSelectorModel.cs
@@ -0,0 +1,54 @@
+﻿namespace Automation_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Skyline.DataMiner.Core.DataMinerSystem.Common;
+	using Skyline.DataMiner.Net.ReportsAndDashboards;
+
+	public class ElementSelectorModel : IElementSelector
+	{
+		private readonly IDms dms;
+		private IDmsElement[] elements;
+		private IDmsElement selectedElement;
+
+		public ElementSelectorModel(IDms dms)
+		{
+			if (dms == null)
+			{
+				throw new ArgumentNullException(nameof(dms));
+			}
+
+			this.dms = dms;
+		}
+
+		public IReadOnlyCollection<IDmsElement> Elements
+		{
+			get
+			{
+				return elements ?? (elements = dms.GetElements().ToArray());
+			}
+		}
+
+		public IDmsElement SelectedElement
+		{
+			get
+			{
+				return selectedElement ?? (selectedElement = Elements.First());
+			}
+
+			set
+			{
+				if(value == SelectedElement)
+				{
+					return;
+				}
+
+				selectedElement = value;
+			}
+		}
+	}
+}
---
+++ Automation_1/IElementSelector.cs
@@ -0,0 +1,16 @@
+﻿namespace Automation_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Skyline.DataMiner.Core.DataMinerSystem.Common;
+
+	public interface IElementSelector
+	{
+		IReadOnlyCollection<IDmsElement> Elements { get; }
+
+		IDmsElement SelectedElement { get; set; }
+	}
+}
