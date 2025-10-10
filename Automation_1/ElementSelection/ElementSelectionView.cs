89570b9102d8ad2785b462caa5f5724b2a57b116
TajanaSA <tajana.salkic@skyline.be>
2025-10-10 13:33:15 +0200

    Added ElementSelectionView dialog with label for the element, dropdown for elements, and continue button, also changed title of the dialog

---
+++ Automation_1/ElementSelection/ElementSelectionView.cs
@@ -0,0 +1,28 @@
+﻿namespace Automation_1.ElementSelection
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+
+	public class ElementSelectionView : Dialog
+	{
+		public ElementSelectionView(IEngine engine) : base(engine)
+		{
+			ElementsDropDown = new DropDown { IsSorted = true, IsDisplayFilterShown = true };
+			ContinueButton = new Button("Continue...");
+			Title = "Pick an element";
+			ElementLabel = new Label("Element");
+			AddWidget(ElementLabel, 0, 0);
+			AddWidget(ElementsDropDown, 0, 1);
+			AddWidget(ContinueButton, 1, 1);
+		}
+
+		public DropDown ElementsDropDown { get; }
+
+		public Button ContinueButton { get; }
+
+		public Label ElementLabel { get; set; }
+	}
+}
