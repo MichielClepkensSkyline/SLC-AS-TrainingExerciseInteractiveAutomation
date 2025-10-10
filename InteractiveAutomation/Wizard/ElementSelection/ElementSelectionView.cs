3ee80acbe8289bce2d591b0e1c590713c407c980
sofiankerkhof <sofian.kerkhof@skyline.be>
2025-10-10 10:20:09 +0200

    Added all files to have a MVP pattern.

--- InteractiveAutomation/Wizard/ElementSelection/ElementSelectionView.cs
+++ InteractiveAutomation/Wizard/ElementSelection/ElementSelectionView.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.Core.DataMinerSystem.Automation;
+using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InteractiveAutomation.Wizard.ElementSelection
 {
-	internal class ElementSelectionView
 	{
 	}
 }
---
+++ InteractiveAutomation/Wizard/ElementSelection/IElementSelectionView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveAutomation.Wizard.ElementSelection
+{
+	using Skyline.DataMiner.Core.DataMinerSystem.Common;
+	using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+	internal interface IElementSelectionView : IDialog
+	{
+
+	}
+}
