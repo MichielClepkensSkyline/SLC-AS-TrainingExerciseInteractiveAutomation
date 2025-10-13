ee17f0e6d54a56ebf14d468fda949cfbd27c9072
TajanaSA <tajana.salkic@skyline.be>
2025-10-13 16:40:59 +0200

    Added ParameterSelectionPresenter with logic for the Continu button and back button also with storeToModel

---
+++ Automation_1/ParameterSelection/ParameterSelectionPresenter.cs
@@ -0,0 +1,50 @@
+﻿namespace Automation_1.ParameterSelection
+{
+	using Automation_1.ElementSelection;
+
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class ParameterSelectionPresenter
+	{
+		private readonly ParameterSelectionView parameterSelectionView;
+		private readonly IElementSelector elementSelector;
+
+		public ParameterSelectionPresenter(ParameterSelectionView view, IElementSelector element)
+		{
+			parameterSelectionView = view ?? throw new ArgumentNullException(nameof(parameterSelectionView));
+			elementSelector = element ?? throw new ArgumentNullException(nameof(elementSelector));
+
+			parameterSelectionView.ContinueButton.Pressed += OnContinueButtonPressed;
+
+			parameterSelectionView.BackButton.Pressed += OnBackButtonPressed;
+		}
+
+		public event EventHandler<EventArgs> Continue;
+
+		public event EventHandler<EventArgs> Back;
+
+
+		private void OnContinueButtonPressed(object sender, EventArgs e)
+		{
+			StoreToModel();
+
+			Continue?.Invoke(this, EventArgs.Empty);
+		}
+
+		private void OnBackButtonPressed(object sender, EventArgs e)
+		{
+			StoreToModel();
+
+			Back?.Invoke(this, EventArgs.Empty);
+		}
+
+		private void StoreToModel()
+		{
+			elementSelector.SelectedParameterId = (int)parameterSelectionView.ParameterId.Value;
+		}
+	}
+}
