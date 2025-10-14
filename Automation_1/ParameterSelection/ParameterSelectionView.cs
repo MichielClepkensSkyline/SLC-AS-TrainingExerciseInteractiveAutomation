5c59a2ed35a30fdafc8efae22779f5976a5a4606
TajanaSA <tajana.salkic@skyline.be>
2025-10-14 10:38:29 +0200

    Added ParameterSelectionView for displaying the widgets

---
+++ Automation_1/ParameterSelection/ParameterSelectionView.cs
@@ -0,0 +1,34 @@
+﻿namespace Automation_1.ParameterSelection
+{
+	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.Net.Messages;
+	using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class ParameterSelectionView : Dialog
+	{
+		public ParameterSelectionView(IEngine engine) : base(engine)
+		{
+
+			ContinueButton = new Button("Continue...");
+			BackButton = new Button("Back");
+			ParameterId = new Numeric();
+			ParameterId.Minimum = 0;
+
+			AddWidget(ParameterId, 0, 0);
+			AddWidget(BackButton, 1, 0);
+			AddWidget(ContinueButton, 1, 1);
+		}
+
+		public Numeric ParameterId { get; }
+
+		public Button ContinueButton { get; }
+
+		public Button BackButton { get; }
+	}
+}
