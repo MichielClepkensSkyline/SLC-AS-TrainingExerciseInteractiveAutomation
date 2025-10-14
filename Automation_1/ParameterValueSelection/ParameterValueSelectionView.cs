be7e95e29cf191b6fc0c689fb1ee97f31f5c87e5
TajanaSA <tajana.salkic@skyline.be>
2025-10-14 10:44:41 +0200

    Added ParameterValueSelectionView with input fields and control buttons, added StringValue (TextBox) and DoubleValue (Numeric) input widgets, included buttons for setting values and navigating back

---
+++ Automation_1/ParameterValueSelection/ParameterValueSelectionView.cs
@@ -0,0 +1,40 @@
+﻿using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_1.ParameterValueSelection
+{
+	public class ParameterValueSelectionView : Dialog
+	{
+		public ParameterValueSelectionView(IEngine engine) : base(engine)
+		{
+			Title = "Set parameter value";
+			BackButton = new Button("Back");
+			SetStringValue = new Button("Set String Value");
+			SetDoubleValue = new Button("Set Double Value");
+			StringValue = new TextBox();
+			DoubleValue = new Numeric() { Decimals = 2, StepSize= 0.01 };
+
+			AddWidget(StringValue, 0, 0);
+			AddWidget(SetStringValue, 0, 1);
+			AddWidget(DoubleValue, 1, 0);
+			AddWidget(SetDoubleValue, 1, 1);
+			AddWidget(BackButton, 2, 0);
+		}
+
+		public Button BackButton { get; }
+
+		public TextBox StringValue { get; }
+
+		public Numeric DoubleValue { get; }
+
+		public Button SetStringValue { get; }
+
+		public Button SetDoubleValue { get; }
+	}
+}
